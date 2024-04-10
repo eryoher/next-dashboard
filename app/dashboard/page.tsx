998ee7310e0285d14235b3b0dbccd74c19d4f446
Ericson Hernandez <eryoher@gmail.com>
2024-04-10 15:41:42 -0400

    :construction: dashboard and invoices.

--- app/dashboard/page.tsx
+++ app/dashboard/page.tsx
@@ -1,9 +1,33 @@
-import React from 'react';
+import React, { Suspense } from 'react';
+import { fMontserrat } from '@/app/ui/fonts';
+import RevenueChart from '@/app/ui/dashboard/revenue-chart';
+import LatestInvoices from '@/app/ui/dashboard/latest-invoices';
+import {
+  CardSkeleton,
+  LatestInvoicesSkeleton,
+  RevenueChartSkeleton,
+} from '../ui/skeletons';
+import CardWrapper from '../ui/dashboard/cards';
 
-type Props = {};
-
-const DashboardPage = (props: Props) => {
-  return <div>DashboardPage</div>;
-};
-
-export default DashboardPage;
+export default async function DashboardPage() {
+  return (
+    <main>
+      <h1 className={`${fMontserrat.className} mb-4 text-xl md:text-2xl`}>
+        Dashboard
+      </h1>
+      <div className="grid gap-6 sm:grid-cols-2 lg:grid-cols-4">
+        <Suspense fallback={<CardSkeleton />}>
+          <CardWrapper />
+        </Suspense>
+      </div>
+      <div className="mt-6 grid grid-cols-1 gap-6 md:grid-cols-4 lg:grid-cols-8">
+        <Suspense fallback={<RevenueChartSkeleton />}>
+          <RevenueChart />
+        </Suspense>
+        <Suspense fallback={<LatestInvoicesSkeleton />}>
+          <LatestInvoices />
+        </Suspense>
+      </div>
+    </main>
+  );
+}
