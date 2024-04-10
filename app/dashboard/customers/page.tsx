6d35c72215a53297838eb988b6c7d68a13367397
Ericson Hernandez <eryoher@gmail.com>
2024-04-10 11:27:49 -0400

    :tada: first commit

---
+++ app/dashboard/customers/page.tsx
@@ -0,0 +1,9 @@
+import React from 'react';
+
+type Props = {};
+
+const CustomerPage = (props: Props) => {
+  return <div>customer</div>;
+};
+
+export default CustomerPage;
---
+++ app/ui/fonts.ts
@@ -0,0 +1,8 @@
+import { Lusitana, Montserrat } from 'next/font/google';
+
+export const fMontserrat = Montserrat({ subsets: ['latin'] });
+
+export const lusitana = Lusitana({
+  weight: ['400', '700'],
+  subsets: ['latin'],
+});
